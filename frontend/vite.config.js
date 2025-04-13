2e1cbfdccb6f53de5913c2ffbb0f53c35a829545
Jashan Mann <jashan@Jashans-MBP-2.lan>
2025-04-12 17:01:12 -0700

    rid of dead code

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,3 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   server: {port:5173}
-// })
 
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
